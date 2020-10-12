b7f7879103fb0fa0db61f9916dc11501e11b123e
Valentina Esteban <valentina.esteban@ug.uchile.cl>
2020-10-12 19:05:25 -0300

    arreglo de la embarrada

--- src/main/java/com/github/cc3002/finalreality/model/weapon/IWeapon.java
+++ src/main/java/com/github/cc3002/finalreality/model/weapon/IWeapon.java
@@ -1,16 +1,10 @@
 package com.github.cc3002.finalreality.model.weapon;
 
 public interface IWeapon {
-<<<<<<< HEAD
 
-=======
->>>>>>> rama-2
     String getName();
     int getDamage();
     int getWeight();
     String getType();
-<<<<<<< HEAD
 
-=======
->>>>>>> rama-2
 }
